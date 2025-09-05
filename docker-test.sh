d31b115aeb9bf9c87e6a6c59082825fcbdb52886
Justin Basinger <justin-basinger@idexx.com>
2025-09-05 15:31:59 -0400

    Running tests in the container

---
+++ docker-test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export IMAGE_NAME='seps'
+
+docker build -t "$IMAGE_NAME" .
+
+docker container rm -f sep_test || true
+docker run -it \
+  --name sep_test \
+  -v "$(pwd)/src:/src" \
+  -w "/src" \
+  -a STDOUT \
+  $IMAGE_NAME ./test.sh >result.log
