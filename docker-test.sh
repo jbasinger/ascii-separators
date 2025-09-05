ddb22801fd2eb379ca47cb02fb1bc814f5d0f664
Justin Basinger <justin-basinger@idexx.com>
2025-09-05 15:34:54 -0400

    This isn't interactive

--- docker-test.sh
+++ docker-test.sh
@@ -5,7 +5,8 @@ export IMAGE_NAME='seps'
 docker build -t "$IMAGE_NAME" .
 
 docker container rm -f sep_test || true
-docker run -it \
+
+docker run --rm \
   --name sep_test \
   -v "$(pwd)/src:/src" \
   -w "/src" \
